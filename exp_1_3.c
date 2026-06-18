801b63850dc62f1f06a58e6225bd65d9f8704241
amarkumarnew1234-cmyk <amarkumarnew1234@gmail.com>
2026-06-18 10:54:51 +0530

    Add files via upload

---
+++ exp_1_3.c
@@ -0,0 +1,12 @@
+//    3.	Convert uppercase string to lowercase using for loop. 
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i;
+    printf("Enter uppercase string:");
+    scanf("%s",str);
+    for(i=0;str[i]!='\0';i++)
+        if(str[i]>='A' && str[i]<='Z')
+            str[i]=str[i]+32;
+    printf("Lowercase string:%s",str);
+}
